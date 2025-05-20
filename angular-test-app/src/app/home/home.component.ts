db691673dbfb1d7ade368e7ad6ad124fcc342097
Paige <gundermann.4@osu.edu>
2025-05-19 22:52:19 -0400

    finished tutorial

--- angular-test-app/src/app/home/home.component.ts
+++ angular-test-app/src/app/home/home.component.ts
@@ -1,24 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HousingLocationComponent} from '../housing-location/housing-location.component';
 import {HousingLocation} from '../housinglocation';
+import {HousingService} from '../housing.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };  
+ housingLocationList: HousingLocation[] = [];
+ housingService: HousingService = inject(HousingService);
+ filteredLocationList: HousingLocation[] = [];
+
+ constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+  
+  filterResults(text: string) {
+    console.log(text);
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
+  }
 }
--- angular-test-app/src/app/housing-location/housing-location.component.ts
+++ angular-test-app/src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import {HousingLocation} from '../housinglocation';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
